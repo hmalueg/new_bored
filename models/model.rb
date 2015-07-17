f7c9354d82468cbd2678e449124ef925d1590b0b
Hailey Malueg <haileym44@Haileys-MacBook-Pro.local>
2015-07-17 12:41:48 -0400

    added lynx

---
+++ models/model.rb
@@ -0,0 +1,23 @@
+$arr ={
+  :cheap => {
+    :in => {
+      :tv => ["<a href='http://www.imdb.com/title/tt1865718/'>watch Gravity Falls</a>", "<a href='http://www.imdb.com/title/tt3107288'>watch The Flash</a>", "<a href='http://www.imdb.com/title/tt2193021/'>watch Arrow </a>", "<a href='http://www.imdb.com/title/tt1475582/'>watch Sherlock</a>", "<a href='http://www.imdb.com/title/tt0436992/'>watch Doctor Who</a>", "<a href='http://www.imdb.com/title/tt0460681/'>watch Supernatural</a>", "<a href='http://www.imdb.com/title/tt0108778/'>watch Friends</a>", "<a href='http://www.imdb.com/title/tt0460649/'>watch How I Met Your Mother</a>"],
+      :book => ["<a href='https://www.goodreads.com/book/show/3.Harry_Potter_and_the_Sorcerer_s_Stone'> read Harry Potter </a>", "<a href='https://www.goodreads.com/book/show/2767052-the-hunger-games'>read The Hunger Games</a>", "<a href='https://www.goodreads.com/book/show/28187.The_Lightning_Thief'>read Percy Jackson</a>", "<a href= 'https://www.goodreads.com/book/show/249747.Artemis_Fowl'> read Artemis Fowl</a>", "<a href= 'https://www.goodreads.com/book/show/34.The_Fellowship_of_the_Ring'> read Lord of the Rings </a>", "<a href='https://www.goodreads.com/book/show/5027092-slathbog-s-gold'>read Adventurers Wanted</a>"]
+      },
+      :out => {
+        :sport => ["play baseball", "play football", "play frisbee", "go swimming"],
+        :nature => ["go for a hike", "climb a tree", "go for a walk", "visit a public park"]
+        }
+    },
+  
+  :expensive => {
+    :in => {
+      :restaurant => ["<a href='https://www.chipotle.com/'>go to Chipotle</a>", "<a href='http://www.benjerry.com/'>go to Ben and Jerry's</a>", "<a href='https://www.shakeshack.com/'>go to Shake Shack</a>", "<a href='http://www.mcdonalds.com/us/en/home.html'>go to McDonalds</a>", "<a href='http://www.starbucks.com/'>go to Starbucks</a>", "<a href='http://sirpizzarestaurants.com/'>go to Sir Pizza</a>"],
+      :movie => ["<a href='http://www.imdb.com/title/tt1392190/'>go see Mad Max</a>", "<a href='http://www.imdb.com/title/tt0120737/'>go see Lord of the Rings</a>", "<a href='http://www.imdb.com/title/tt0903624/'>go see The Hobbit</a>", "<a href='http://www.imdb.com/title/tt2096673/'>go see Inside Out</a>", "<a href='http://www.imdb.com/title/tt0082971/'>go see Indiana Jones</a>", "<a href='http://www.imdb.com/title/tt2395427/'>go see The Avengers</a>", "<a href='http://www.imdb.com/title/tt0109830/'>go see Forrest Gump</a>"]
+      },
+    :out => {
+      :sightseeing => ["visit the zoo", "go to a public pool", "visit a museum", "visit an art gallery"],
+      :shopping => ["visit a local salon or spa", "visit a local mall", "treat yo'self", "buy a suprise present for your friend ", "go to Sunset Place", "go to the Falls", "go to Dadeland Mall"]
+      }
+    }
+  }
