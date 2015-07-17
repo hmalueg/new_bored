3148182f1ce0834bacbf34445c3ceb893c6837ab
draconaspark <draconaspark@gmail.com>
2015-07-17 14:10:53 +0000

    final

--- application_controller.rb
+++ application_controller.rb
@@ -19,28 +19,28 @@ class MyApp < Sinatra::Base
       erb :cheap_and_inside
     elsif params[:money]=="no" && params[:nature]=="outside"
       erb :cheap_and_outside
-    elsif params[:money]=="yes" && params[:nature]=="iinside"
+    elsif params[:money]=="yes" && params[:nature]=="inside"
       erb :money_and_inside
     else params[:money]=="yes" && params[:nature]=="outside"
       erb :money_and_outside
     end
   end
   post '/results' do 
-   if params[:things]=="tv show"
+   if params[:thing]=="tv show"
     @output = ($arr[:cheap][:in][:tv][rand($arr[:cheap][:in][:tv].length)])
-   elsif params[:things]=="book"
+   elsif params[:thing]=="book"
      @output = ($arr[:cheap][:in][:book][rand($arr[:cheap][:in][:book].length)])
-   elsif params[:things]=="sport"
+   elsif params[:thing]=="sport"
      @output = ($arr[:cheap][:out][:sport][rand($arr[:cheap][:out][:sport].length)])
-   elsif params[:things]=="nature"
+   elsif params[:thing]=="nature"
      @output = ($arr[:cheap][:out][:nature][rand($arr[:cheap][:out][:nature].length)])
-   elsif params[:things]=="resteraunt"
-     @output = ($arr[:expensive][:in][:resteraunt][rand($arr[:expensive][:in][:restaurant].length)])
-   elsif params[:things]=="movie"
+   elsif params[:thing]=="restaurant"
+     @output = ($arr[:expensive][:in][:restaurant][rand($arr[:expensive][:in][:restaurant].length)])
+   elsif params[:thing]=="movie"
      @output = ($arr[:expensive][:in][:movie][rand($arr[:expensive][:in][:movie].length)])
-   elsif params[:things]=="sightseeing"
+   elsif params[:thing]=="sightseeing"
      @output =($arr[:expensive][:out][:sightseeing][rand($arr[:expensive][:out][:sightseeing].length)])
-   else params[:things]=="shopping"
+   else params[:thing]=="shopping"
      @output =($arr[:expensive][:out][:shopping][rand($arr[:expensive][:out][:shopping].length)])
    end
     erb :results
--- models/model.rb
+++ models/model.rb
@@ -1,8 +1,8 @@
 $arr ={
   :cheap => {
     :in => {
-      :tv => ["Gravity Falls", "The Flash", "Arrow", "Sherlock", "Doctor Who", "Supernatural", "Friends", "How I Met Your Mother"],
-      :book => ["Harry Potter", "The Hunger Games", "Percy Jackson", "Artemis Fowl", "Lord of the Rings", "Adventurers Wanted"]
+      :tv => ["watch Gravity Falls", "watch The Flash", "watch Arrow", "watch Sherlock", "watch Doctor Who", "watch Supernatural", "watch Friends", "watch How I Met Your Mother"],
+      :book => ["<a href='https://shop.pottermore.com/en_us/?utm_source=usexperiencesite&utm_medium=experience&utm_campaign=sohpanel'> read Harry Potter </a>", "read The Hunger Games", "read Percy Jackson", "<a href= 'http://www.artemisfowl.com/'> read Artemis Fowl</a>", "<a href= 'http://www.bookspopular.com/J_R_R_Tolkien/The_Fellowship_of_the_Ring/01.html'> read Lord of the Rings </a>", "read Adventurers Wanted"]
       },
       :out => {
         :sport => ["play baseball", "play football", "play frisbee", "go swimming"],
@@ -12,11 +12,11 @@ $arr ={
   
   :expensive => {
     :in => {
-      :restaurant => ["Chipotle", "Ben and Jerry's", "Shake Shack", "McDonalds", "Starbucks", "Sir Pizza"],
-      :movie => ["Mad Max", "Lord of the Rings", "The Hobbit", "Inside Out", "Indiana Jones", "The Avengers", "Forrest Gump"]
+      :restaurant => ["go to Chipotle", "go to Ben and Jerry's", "go to Shake Shack", "go to McDonalds", "go to Starbucks", "go to Sir Pizza"],
+      :movie => ["go see Mad Max", "go see Lord of the Rings", "go see The Hobbit", "go see Inside Out", "go see Indiana Jones", "go see The Avengers", "go see Forrest Gump"]
       },
     :out => {
-      :sightseeing => ["the zoo", "a public pool", "a museum", "an art gallery"],
+      :sightseeing => ["visit the zoo", "go to a public pool", "visit a museum", "visit an art gallery"],
       :shopping => ["visit a local salon or spa", "visit a local mall", "treat yo'self", "buy a suprise present for your friend ", "go to Sunset Place", "go to the Falls", "go to Dadeland Mall"]
       }
     }
